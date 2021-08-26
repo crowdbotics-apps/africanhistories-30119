58d302be92aecf7a0403f45ad7a58991ad2dc140
Crowdbotics <team@crowdbotics.com>
2021-08-26 09:43:04 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "africanhistories_30119Identifier",
+  url: "https://africanhistories_30119Identifier.botics.co",
+  api: "https://africanhistories_30119Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
